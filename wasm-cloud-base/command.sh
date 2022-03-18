a3332fc71c79c6adcc3de62f1cd99cacbce4a08c
Evan Mattiza <emattiza@gmail.com>
2022-03-18 21:15:13 +0000

    add registry localhost to /etc/hosts

--- wasm-cloud-base/command.sh
+++ wasm-cloud-base/command.sh
@@ -1,4 +1,5 @@
 export GP_HTTPS_HOST=$(gp url 4000)
 export GP_HOST=$(echo "$GP_HTTPS_HOST" | sed s#https://##)
 export DIR=$(pwd)
+echo "127.0.0.1\tregistry" | sudo tee -a /etc/hosts > /dev/null
 docker-compose -f wasm-cloud-base/docker-compose.yml up
